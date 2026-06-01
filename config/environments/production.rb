c94e026a37263fc79f1d18f39828af57c36032db
Jose David <josedavid1velazquezcruz@gmail.com>
2026-06-01 04:16:47 -0600

    Configurar correo Gmail en produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,22 @@ config.action_mailer.default_url_options = {
   host: "ai-link-production.up.railway.app",
   protocol: "https"
 }
+config.action_mailer.default_url_options = {
+  host: "ai-link-production.up.railway.app",
+  protocol: "https"
+}
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_APP_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
 end
