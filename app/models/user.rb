12f14ac519d260d14ff6ce84b246abc0146d1b73
Jose David <josedavid1velazquezcruz@gmail.com>
2026-06-02 15:59:42 -0600

    temporarily disable email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_one_attached :avatar
 
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable,
-         :confirmable
+devise :database_authenticatable,
+       :registerable,
+       :recoverable,
+       :rememberable,
+       :validatable
 
 end
