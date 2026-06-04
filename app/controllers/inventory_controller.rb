fcf7221a12d2c4e045356585688bf05f17aff897
Jose David <josedavid1velazquezcruz@gmail.com>
2026-06-03 23:18:49 -0600

    redesign inventory with Haru

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,12 +1,11 @@
 class InventoryController < ApplicationController
-
   before_action :authenticate_user!
 
   def index
-    @products = current_user.products
+    @products = current_user.products.order(created_at: :desc)
     @total_products = @products.count
     @total_stock = @products.sum(:quantity)
-    @low_stock = @products.where("quantity <= ?", 5).count
+    @low_stock = @products.where("quantity <= ? AND quantity > ?", 5, 0).count
   end
 
   def sold
@@ -14,24 +13,25 @@ class InventoryController < ApplicationController
     sold_quantity = params[:sold_quantity].to_i
 
     if sold_quantity > 0
-      product.quantity = product.quantity - sold_quantity
+      product.quantity = product.quantity.to_i - sold_quantity
       product.quantity = 0 if product.quantity < 0
       product.save
     end
 
-    redirect_to "/inventory/index"
+    redirect_to inventory_index_path
   end
 
   def sold_out
     product = current_user.products.find(params[:product_id])
     product.update(quantity: 0)
 
-    redirect_to "/inventory/index"
+    redirect_to inventory_index_path
   end
-def delete
-  product = current_user.products.find(params[:product_id])
-  product.destroy
 
-  redirect_to "/inventory/index"
-end
-end
+  def delete
+    product = current_user.products.find(params[:product_id])
+    product.destroy
+
+    redirect_to inventory_index_path
+  end
+end 
