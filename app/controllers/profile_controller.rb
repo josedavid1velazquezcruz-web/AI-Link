77ddfa6940ffb2e27203f2b50a2f032ff8356053
Jose David <josedavid1velazquezcruz@gmail.com>
2026-06-04 01:36:41 -0600

    add profile styles

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,18 +1,21 @@
 class ProfileController < ApplicationController
-
   before_action :authenticate_user!
 
   def index
+    @total_campaigns = current_user.campaigns.count
+    @total_products = current_user.products.count
+    @total_stock = current_user.products.sum(:quantity)
   end
 
   def update
-    current_user.update(username: params[:username])
+    current_user.update(
+      username: params[:username]
+    )
 
     if params[:avatar].present?
       current_user.avatar.attach(params[:avatar])
     end
 
-    redirect_to "/profile/index"
+    redirect_to profile_index_path
   end
-
 end
